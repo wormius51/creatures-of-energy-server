39fa8362bd59571c3fbd119e63dd76ac46996b18
Eyal Hazor <eyalhazor1@gmail.com>
2019-02-28 01:53:40 +0200

    check whats wrong

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || '5000';
+console.log('port: ' + PORT);
 const app = express();
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
 
@@ -18,20 +19,3 @@ app.use(session({secret : randomString, resave : false, saveUninitialized : true
 
 app.use('/session',require('./session'));
 app.use('/match-making',require('./match-making'));
-
-
-/*const server = require('http').Server(app);
-const io = require('socket.io')(server);
-io.on('connection',client => {
-    console.log('client connection: ' + client);
-    client.on('match-start', data => {
-        console.log(data);
-    })
-    client.on('make-move', data => {
-        console.log(data);
-    });
-
-    client.emit('make-move',client.id);
-});
-*/
-//module.exports.io = io;
