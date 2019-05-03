b8ad84d9681da51693b9c5d13e6362d2b693bcf5
Eyal Hazor <eyalhazor1@gmail.com>
2019-05-03 16:06:07 +0300

    cross origin

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || '5000';
 const app = express();
+app.use(cors());
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
 
 app.use(bodyParser.json());
